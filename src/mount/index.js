ba2410683112d13be24e8d37131972433dd5eba4
Devin Canterberry <devin@canterberry.cc>
2019-10-29 20:58:38 -0700

    🔧 Add step to generate code-level documentation
    Documentation can be a divisive subject amongst developers.
    
     * Some developers consider code-level documentation to be
       a waste of time.
    
     * Some developers consider code-level documentation to be
       an essential part of developer and project hygiene.
    
    The truth is, both opinions have merits. As with any practice
    you adopt as a developer, you should always be asking yourself,
    "Why am I doing this?" and "What is the value of doing this?"
    
    Investing your time on creating and maintaining good code-level
    documentation is only worth it if you, or other developers, are
    **reading** that documentation. A close-knit team working in a
    single project together may know the code well enough that the
    documentation may seem pointless. Also, if there is a chance that
    documentation may drift and become inaccurate or untrustworthy,
    then even new developers onboarding onto the project may shy away
    from it as a precaution against being potentially misinformed.
    
    If you are developing a reusable package, then you almost
    certainly should be writing code-level documentation on, at least,
    the parts of your code you expect other developers to use. This
    is how most developers will decide whether to use your package,
    and will service as a perpetual reference on *how* they exercise it.
    
    If you are developing an app (client, server, or otherwise), then
    the benefits of code-level documentation are less clear. Since
    other developers are not consuming your code, the audience for your
    documentation is other developers *contributing* directly to the
    app. These developers have access to the source code itself, and
    are capable of navigating its implementation and/or consulting
    directly with the other app developers to share knowledge. In this
    case, the practice of writing and maintaining code-level documentation
    is a decision best left to the team, based on its merits to how
    the team likes to work.
    
    When I refer to documentation in this context, by the way, I mean
    API reference documentation. Typically, for each documented function:
    
     * A brief description of what the function is designed to accomplish.
    
     * A list of the arguments expected by the function, including each
       argument's name, type, and a brief explanation.
    
     * A description of what the function returns, including the return
       type.
    
    JSDoc is a common way of describing functions and other units with
    code-level block comments, so this commit integrates that.
    
    To illustrate what this documentation looks like, I've documented
    the `#mount()` function. I've also changed the name of its `element`
    argument to `node`, since this argument can be any DOM node, not just
    an element.
    
    Finally, I've added a .gitignore entry for **/docs/**, which is where
    we've elected to write the generated documentation from JSDoc.

--- src/mount/index.js
+++ src/mount/index.js
@@ -1,4 +1,12 @@
-export default function mount(element, document) {
+/**
+ * Mounts a node to the document body, within a container <div>.
+ *
+ * @param {Node} node - The DOM node to be mounted.
+ * @param {Document} document - The document to which the given node should be mounted.
+ *
+ * @returns {Element} - Returns the injected container element for the injected node.
+ */
+const mount = (node, document) => {
   const root = document.querySelector('body');
 
   let container = root.querySelector('div');
@@ -8,9 +16,11 @@ export default function mount(element, document) {
   }
 
   container = document.createElement('div');
-  container.appendChild(element);
+  container.appendChild(node);
 
   root.appendChild(container);
 
   return container;
-}
+};
+
+export default mount;
--- src/mount/spec.js
+++ src/mount/spec.js
@@ -8,22 +8,22 @@ import mount from '.';
 describe('#mount()', () => {
   it('injects a <div> to the document body', () => {
     const { window: { document } } = new JSDOM('<html><body></body></html>');
-    const element = document.createElement('i');
-    mount(element, document);
+    const node = document.createElement('i');
+    mount(node, document);
     expect(document.querySelector('body > div')).not.to.be.null;
   });
 
-  it('places the given element into the injected <div>', () => {
+  it('places the given node into the injected <div>', () => {
     const { window: { document } } = new JSDOM('<html><body></body></html>');
-    const element = document.createElement('i');
-    mount(element, document);
+    const node = document.createElement('i');
+    mount(node, document);
     expect(document.querySelector('body > div > i')).not.to.be.null;
   });
 
   it('replaces the existing <div>, if any', () => {
     const { window: { document } } = new JSDOM('<html><body><div></div></body></html>');
-    const element = document.createElement('i');
-    mount(element, document);
+    const node = document.createElement('i');
+    mount(node, document);
     expect(document.querySelectorAll('body > div')).to.have.length(1);
   });
 });
